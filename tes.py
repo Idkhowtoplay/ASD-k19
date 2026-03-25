709d7d23a7db81790013bc537f5733fbfc9a5075
idkhowtoplay <akbarzainuri29@gmail.com>
2026-03-25 09:16:21 +0700

    society

---
+++ tes.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, parent1, parent2):
+        self.name = name
+        self.parent1 = parent1
+        self.parent2 = parent2
+
+class FamilyTree:
+    def __init__(self):
+        self.head = None
+
+        
+
+    
